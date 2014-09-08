c7a75ff6cb74dda08a74546b4fb12c745a7518fc
sojamo <andi@sojamo.de>
2014-09-08 15:30:21 +0800

    resources update

--- resources/check-examples.sh
+++ resources/check-examples.sh
@@ -1,5 +1,5 @@
 for d in $HOME/Documents/workspace/controlp5/examples/**/*/; do
 	# echo "$d"
-	java -cp .:../../../distribution/tmp/controlP5/library/controlP5.jar:$HOME/Documents/workspace/libs/core.jar AddMethodList $d
+	java -cp .:../distribution/tmp/controlP5/library/controlP5.jar:$HOME/Documents/workspace/libs/core.jar:./addons/AddMethodList AddMethodList $d
 	processing-java --sketch="$d" --output=/tmp/processing-test --force --build
 done
