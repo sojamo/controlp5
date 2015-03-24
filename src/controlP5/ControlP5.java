d5c22c4be723a71701efa13d2a8ee147bab04c7a
sojamo <andi@sojamo.de>
2015-03-24 12:43:40 +0800

    release for DOI

--- src/controlP5/ControlP5.java
+++ src/controlP5/ControlP5.java
@@ -98,7 +98,7 @@ public class ControlP5 extends ControlP5Base {
 	/**
 	 * @exclude
 	 */
-	@ControlP5.Invisible public static final String VERSION = "2.2.3";// "##version##";
+	@ControlP5.Invisible public static final String VERSION = "2.2.4";// "##version##";
 
 	/**
 	 * @exclude
